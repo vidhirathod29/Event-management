f5e7ee42f5ba1c330557a6e553ad6b3edc44cf2c
vidhirathod29 <vidhirathod.shivinfotech@gmail.com>
2024-04-15 19:02:19 +0530

    changes: made changes in validation remove empty() for unnecessary fields

--- validation/eventValidation.js
+++ validation/eventValidation.js
@@ -6,39 +6,31 @@ module.exports = {
       'string.base': 'Event name should be type of string',
       'string.empty': 'Event name should not be empty',
       'string.max': 'Event name should have maximum length of 50',
-      'any.required': 'Event is a required',
     }),
     event_description: Joi.string().empty().max(250).required().messages({
       'string.base': 'Event description should be type of string',
       'string.empty': 'Event description should not be empty',
       'string.max': 'Event description should have maximum length of 250',
-      'any.required': 'Event is a required',
     }),
   }),
 
   updateEvent: Joi.object({
-    event_name: Joi.string().empty().max(50).messages({
+    event_name: Joi.string().optional().max(50).messages({
       'string.base': 'Event name should be type of string',
-      'string.empty': 'Event name should not be empty',
       'string.max': 'Event name should have maximum length of 50',
     }),
-    event_description: Joi.string().empty().max(250).messages({
+    event_description: Joi.string().optional().max(250).messages({
       'string.base': 'Event description should be type of string',
-      'string.empty': 'Event description should not be empty',
       'string.max': 'Event description should have maximum length of 250',
     }),
   }),
 
   listOfEvent: Joi.object({
-    condition: Joi.object().optional().empty().messages({
+    condition: Joi.object().optional().messages({
       'object.base': 'Condition should be type of an object',
-      'object.optional': 'Condition is an optional field',
-      'object.empty': 'Condition should not be empty',
     }),
-    pageSize: Joi.number().optional().empty().messages({
+    pageSize: Joi.number().optional().messages({
       'number.base': 'pageSize should be type of a number',
-      'number.optional': 'PageSize is an optional field',
-      'number.empty': 'pageSize should not be empty',
     }),
   }),
 };
