1c58fd30395815495e69f63342e8c5836e26fd60
vidhirathod29 <vidhirathod.shivinfotech@gmail.com>
2024-04-15 18:51:55 +0530

    changes: made changes in validation of events

--- validation/eventValidation.js
+++ validation/eventValidation.js
@@ -21,23 +21,23 @@ module.exports = {
       'string.base': 'Event name should be type of string',
       'string.empty': 'Event name should not be empty',
       'string.max': 'Event name should have maximum length of 50',
-      'any.required': 'Event is a required',
     }),
     event_description: Joi.string().empty().max(250).messages({
       'string.base': 'Event description should be type of string',
       'string.empty': 'Event description should not be empty',
       'string.max': 'Event description should have maximum length of 250',
-      'any.required': 'Event is a required',
     }),
   }),
 
   listOfEvent: Joi.object({
     condition: Joi.object().optional().empty().messages({
       'object.base': 'Condition should be type of an object',
+      'object.optional': 'Condition is an optional field',
       'object.empty': 'Condition should not be empty',
     }),
     pageSize: Joi.number().optional().empty().messages({
       'number.base': 'pageSize should be type of a number',
+      'number.optional': 'PageSize is an optional field',
       'number.empty': 'pageSize should not be empty',
     }),
   }),
